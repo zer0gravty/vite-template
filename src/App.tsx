7f4294204c1193bf291735c5a21bd93b3c3b08fe
Frank Vamos, Jr. <fvamos@gmail.com>
2025-02-14 01:08:11 -0500

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='app-ctr w-full h-dvh flex'>
+        <h1 className="text-2xl font-bold flex w-full justify-center items-center">Hello.</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
