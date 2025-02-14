096c166bc3fbb2df51387ec54ddd893e6cca8a8f
Frank Vamos, Jr. <fvamos@gmail.com>
2025-02-14 02:29:18 -0500

    Add Biome and remove ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Button } from './components/ui/button';
+import { Button } from "./components/ui/button";
 
 function App() {
-	return (
-		<div className="app-ctr w-full h-dvh flex">
-			<div className="flex justify-center items-center w-full">
-				<Button>Click me.</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="app-ctr w-full h-dvh flex">
+      <div className="flex justify-center items-center w-full">
+        <Button>Click me.</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
